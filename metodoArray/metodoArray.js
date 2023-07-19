61a8c1f322bf24ea1e5f9522ffccdf499e880ff2
IasminFernandes <126726115+IasminCQFernandes@users.noreply.github.com>
2023-07-19 17:34:03 -0300

    aula metodo arrayII

--- metodoArray/metodoArray.js
+++ metodoArray/metodoArray.js
@@ -69,3 +69,156 @@ lista.splice(1,2,'f')
 //altera indice 1, remove 2 posições(o indice 1 e o proximo) e insere o f na nova
 
 console.log(lista);
+
+
+
+
+
+
+//callback = função que recebe como argumento outra função
+const imprimirNome = (nome) =>{
+    console.log(nome);
+}
+
+
+//setTimeout - cria um dalay antes de chamar a função
+
+setTimeout(imprimirNome('iasmin'), 2000)
+//é possivel chamar uma função dentro de outra
+
+setTimeout(() =>{
+    console.log('teste');
+}, 1000)
+//tambem é possivel criar uma arrow função dentro da outra
+
+setTimeout(function teste(){
+    console.log('teste');
+}, 1000)
+//tambem é possivel criar uma função comum
+
+
+
+//every testa todos os elementos e se todos os elementos condizer com o parametro passado ele retorna true, caso algum não condiza retorna false
+const frutas = ['abacaxi', 'maça', 'banana', 'melancia']
+
+const resultado = frutas.every(function resultado(elemento) {
+    return elemento === 'banana';
+    
+})
+
+console.log(resultado);
+
+//setInterval = chama a função diversas vezes durante um tempo definido
+setInterval(function saudacao(){
+    console.log('ola');
+}, 2000)
+
+
+//exercicio
+const usuarios = [
+    {nome: "Ian", 
+    idade:25},
+
+    {nome: "Iasmin", 
+    idade:21},
+
+    {nome: "Ana", 
+    idade:18},
+
+    {nome: "Lara", 
+    idade:18},
+
+]
+
+const fiscalizarFesta = (arrayObj) => {
+    const resultado = arrayObj.every((elemento) => {
+        return elemento.idade> 17
+    });
+
+    if(resultado){
+        console.log('Festa liberada!');
+    }else{
+        console.log("Não podemos festar");
+    }
+}
+
+fiscalizarFesta(usuarios)
+
+
+
+//some testa todos os elementos e se pelo menos um dos elementos condizer com o parametro passado ele retorna true, caso algum não condiza retorna false
+const frutas2 = ['abacaxi', 'maça', 'banana', 'melancia']
+
+const resultadoSome = frutas2.some(function resultado(elemento) {
+    return elemento === 'banana';
+    
+})
+console.log(resultadoSome)
+
+//exercicio
+const arquivos =[
+    'teste.txt',
+    'foto.png',
+    'contrato.doc',
+    'instalador.bat'
+]
+
+const antiVirus = (arrayArquivos) =>{
+    const resultado = arrayArquivos.some((arquivo)=>{
+        const existeExtensao = arquivo.indexOf('.bat')
+
+        return existeExtensao !== -1 
+    })
+
+    if (resultado) {
+        console.log('virus detectado');
+    }else{
+        console.log('sem virus');
+    }
+}
+antiVirus(arquivos)
+
+
+//find retorna os dados de uma ocorrencia ou seja retorna o mesmo se encontrar caso não encontre retorna um undefined
+const novosUsuarios = [
+    {nome: "Ian", 
+    idade:25},
+
+    {nome: "Iasmin", 
+    idade:21},
+
+    {nome: "Ana", 
+    idade:18},
+
+    {nome: "Lara", 
+    idade:18},
+
+]
+
+const novoResultado = novosUsuarios.find((novosUsuarios) => {
+    return novosUsuarios.nome === 'Ian'
+})
+console.log(novoResultado);
+
+//findIndex - encontra e retorna o index onde encontrar 
+const novoResultado2 = novosUsuarios.findIndex((novosUsuarios) => {
+    return novosUsuarios.nome === 'Ian'
+})
+console.log(novoResultado2);
+
+
+//filter - cria um novo array sem alterar o original, com os elementos com base na condição callback passada no argumento
+const numerosArray = [1,2,3,4,5,6,7,8,9,0]
+
+const resultadoNumeros = numerosArray.filter((numero)=>{
+    return numero <= 3;
+})
+
+console.log(resultadoNumeros);
+
+//map = para cada elemento do array retorna um novo array, com os resultados da função passada como parâmetro
+const numerosMap = [2,4,6]
+const novoArray = numerosMap.map((numero)=>{
+    return numero*2
+})
+console.log(novoArray);
---
+++ metodoArray/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+const numerosMap = [2,4,6]
+const novoArray = numerosMap.map((numero)=>{
+    return numero*2
+})
+console.log(novoArray);
